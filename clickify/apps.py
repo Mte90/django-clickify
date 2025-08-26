c33062b03c1084438dab72611ec7a58886f7ff12
Romjan Ali <romjanvr5@gmail.com>
2025-08-26 19:24:55 +0600

    feat(geoip): Load GeoIP database on app startup

--- clickify/apps.py
+++ clickify/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.conf import settings
+import geoip2.database
 
 
 class ClickifyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'clickify'
-    verbose_name = 'Click Tracker for Downloads'
+
+    def ready(self):
+        if not hasattr(settings, 'GEOIP_PATH'):
+            return
+        try:
+            self.geoip_reader = geoip2.database.Reader(settings.GEOIP_PATH)
+        except (FileNotFoundError, ValueError):
+            self.geoip_reader = None
