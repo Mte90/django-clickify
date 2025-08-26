ee596ba89c0d6fe1a6ab3cd3afe38e327e80cf55
Romjan Ali <romjanvr5@gmail.com>
2025-08-26 19:36:24 +0600

    refactor(views): Use get_geolocation utility

--- clickify/views.py
+++ clickify/views.py
@@ -1,9 +1,9 @@
+from .utils import get_geolocation
 from ipware import get_client_ip
 from django.http import FileResponse, Http404
 from django.conf import settings
 from .models import DownloadClick
 from django_ratelimit.decorators import ratelimit
-import geoip2.database
 import os
 
 
@@ -11,16 +11,10 @@ import os
 def track_download(request, file_path):
     ip, is_routable = get_client_ip(request)
     user_agent = request.META.get("HTTP_USER_AGENT", "")
-    country = city = ""
 
     # GeoIP
-    try:
-        reader = geoip2.database.Reader(settings.GEOIP_PATH)
-        response = reader.city(ip)
-        country = response.country.name
-        city = response.city.name
-    except Exception:
-        pass
+    country, city = get_geolocation(ip)
+   
 
     # Saveto DB
     DownloadClick.objects.create(
